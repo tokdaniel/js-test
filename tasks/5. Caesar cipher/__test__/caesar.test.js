594838951a107964908986354708d29765ea1da3
toki <tokdaniel4@gmail.com>
2018-03-14 10:41:25 +0100

    added rotation/wrap-around unit test to caesar cipher

--- tasks/5. Caesar cipher/__test__/caesar.test.js
+++ tasks/5. Caesar cipher/__test__/caesar.test.js
@@ -5,13 +5,18 @@ describe('encryptCaesar() function', () => {
 
   const input = 'GAMEOFTHRONES';
 
-  it('should encrypt \'GAMEOFTHRONES\' to HBNFPGUISPOFT with key 1', function() {
+  it('should encrypt \'GAMEOFTHRONES\' to HBNFPGUISPOFT with key 1', function () {
 
     expect(encryptCaesar('GAMEOFTHRONES', 1)).to.equal('HBNFPGUISPOFT');
   });
 
-  it('should encrypt \'GAMEOFTHRONES\' to JDPHRIWKURQHV with key 3', function() {
+  it('should encrypt \'GAMEOFTHRONES\' to JDPHRIWKURQHV with key 3', function () {
 
     expect(encryptCaesar('GAMEOFTHRONES', 3)).to.equal('JDPHRIWKURQHV');
+  });
+
+  it('should rotate/wrap around letters', function () {
+
+    expect(encryptCaesar('XYZ', 4)).to.equal('BCD');
   });
 });
