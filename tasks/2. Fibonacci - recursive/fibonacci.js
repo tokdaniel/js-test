cba8d5a2479c711ee8f02e9862a74009846c5f81
toki <tokdaniel4@gmail.com>
2018-03-15 11:29:33 +0100

    fibonacci again

--- tasks/2. Fibonacci - recursive/fibonacci.js
+++ tasks/2. Fibonacci - recursive/fibonacci.js
@@ -1,8 +1,3 @@
 export function getFibonacciUntil(n) {
-  function getFibonacci(i) {
-    if (i === 0 || i === 1) return i;
-    if (i >= 2) return getFibonacci(i - 2) + getFibonacci(i - 1);
-  }
-  if (typeof n !== 'number') return false;
-  return Array.from({ length: n }, (_, index) => getFibonacci(index));
+  //TODO write your code here, remember it should be recursive
 }
