ff71dc2863a87a47ca4d3d7a269dc663977442e2
Juju1980 <88070381+Juju1980@users.noreply.github.com>
2021-08-28 08:43:18 -0400

    Update fizzbuzz.js
    Caison Lee

--- tasks/4. FizzBuzz/fizzbuzz.js
+++ tasks/4. FizzBuzz/fizzbuzz.js
@@ -1,3 +1,16 @@
 export function getFizzBuzzUntil(n) {
   // TODO: write your code here
+  let arr_fb = [];
+  for(let i = 1; i <= n; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+      arr_fb[]="FizzBuzz";
+    }else if (i % 3 === 0){
+	  arr_fb[]="Fizz";
+    }else if (i % 5 === 0){
+	  arr_fb[]="Buzz";
+    }else{
+      arr_fb[]=i;
+    }
+  }
+  return arr_fb;
 }
