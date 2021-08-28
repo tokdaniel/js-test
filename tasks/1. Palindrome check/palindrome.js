8806a4c5fc7fa1170f3ff8504f24b017ae934030
Juju1980 <88070381+Juju1980@users.noreply.github.com>
2021-08-27 23:25:13 -0400

    Update palindrome.js

--- tasks/1. Palindrome check/palindrome.js
+++ tasks/1. Palindrome check/palindrome.js
@@ -1,3 +1,7 @@
 export function isPalindrome(inputString) {
   // TODO: write your code here
+  var str_regex = /[\W_]/g;
+  var str_lowreg = str.toLowerCase().replace(str_regex, ''); 
+  var str_reverse = str_lowreg.split('').reverse().join(''); 
+  return str_reverse === str_lowreg;
 }
