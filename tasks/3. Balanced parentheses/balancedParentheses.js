7073b414ca735068bac8d1c98ea2990c393207f2
Juju1980 <88070381+Juju1980@users.noreply.github.com>
2021-08-28 08:27:24 -0400

    Update balancedParentheses.js
    Caison Lee

--- tasks/3. Balanced parentheses/balancedParentheses.js
+++ tasks/3. Balanced parentheses/balancedParentheses.js
@@ -1,3 +1,27 @@
 export function areParenthesesBalanced(inputString) {
   // TODO: write your code here
+   let stack = [];
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+
+    for (let i = 0; i < inputString.length; i++) {
+
+        
+        if (inputString[i] === '(' || inputString[i] === '{' || inputString[i] === '[' ) {
+            stack.push(inputString[i]);
+        }
+        
+        else {
+            let last = stack.pop();
+
+            if (inputString[i] !== map[last]) {return false};
+        }
+    }
+    
+        if (stack.length !== 0) {return false};
+
+    return true;
 }
